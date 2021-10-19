d5b4d75450c97899e4ea4dd3a6cd430e15e327b2
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-10-20 07:59:46 +0800

    Initial Commit

---
+++ Automatic-reporting-of-car-recordersUITests/Automatic_reporting_of_car_recordersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Automatic_reporting_of_car_recordersUITestsLaunchTests.swift
+//  Automatic-reporting-of-car-recordersUITests
+//
+//  Created by Magic Keegan on 10/20/21.
+//
+
+import XCTest
+
+class Automatic_reporting_of_car_recordersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
